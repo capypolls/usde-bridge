4d2d8444dae007d9aa05ee4781f686e3f8e77d1e
degencodebeast <newfirereloaded@protonmail.com>
2024-12-31 13:16:57 +0100

    fixed build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+//import { WagmiConfig } from 'wagmi'
+//import { config } from '@/lib/config/web3'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* <WagmiConfig config={config}>
+          
+        </WagmiConfig> */}
       </body>
     </html>
   );
